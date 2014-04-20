868417b026e1abfa2e04af3b0dc0b7ac06d3a2b5
John Surveyor <jms@internode.on.net>
2014-04-20 22:36:36 +1000

    Comment & default val update

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
 
 ## makeCacheMatrix: convert a matrix to an object with cached inverse
 #  the cached matrix object has the following methods
-#   cx <- makeCacheMatrix( x )  # covert x to an object with a cache
+#   cx <- makeCacheMatrix( x )  # Construct x to an object with a cache
 #   cx$get()			# return the matrix
 #   cx$set(x)			# Put a different matrix into the object
-#   cx$getinverse()		# return the cache of the inverse
+#   cx$getinverse()		# Return the cache of the inverse
 #   cx$setinverse()		# Put the inverse matrix into the cache
 #
 # Note:  The cache is only set when cx$setinverse() is called
-#    cx$set(x) will clear the cache
+#    cx$set(x) will clear the cache as a side-effect
 
-makeCacheMatrix <- function(x = matrix(1)) {
+makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
                 x <<- y
@@ -47,5 +45,5 @@ cacheSolve <- function(x, ...) {
         m
 }
 
-## Incidentalyy, IMHO in would be better to merge the functionality of
-# cacheSolve() into cx$getinverse() . but I do what I am told!
+## Incidentally, IMHO it would be better to merge the functionality of
+# cacheSolve() into cx$getinverse() - but I do what I am told!
